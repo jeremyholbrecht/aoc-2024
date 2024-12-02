41b66c0e962e5a1b07ad0795fd062f405d6752c0
jeremyholbrecht <griffley@proton.me>
2024-12-02 14:42:48 +0100

    solved day 1 part 1

--- 1/main.go
+++ 1/main.go
@@ -1,26 +1,64 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"math"
-	"sort"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
 )
 
 func main() {
 
-	nums1 := []int{3, 4, 2, 1, 3, 3}
-	nums2 := []int{4, 3, 5, 3, 9, 3}
+	file, err := os.Open("data.txt")
+	if err != nil {
+		log.Fatal("error reading file... ", err)
+	}
 
-	sort.Ints(nums1)
-	sort.Ints(nums2)
+	defer file.Close()
+
+	r := bufio.NewReader(file)
+
+	var list1 = []int{}
+	var list2 = []int{}
+	for {
+		line, _, err := r.ReadLine()
+		if len(line) > 0 {
+			// split into substring based on whitespace
+			fields := strings.Fields(string(line))
+
+			// convert string data to int
+			fieldToInt1, err := strconv.Atoi(fields[0])
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fieldToInt2, err := strconv.Atoi(fields[1])
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			// add data to slice list1 & list2
+			list1 = append(list1, fieldToInt1)
+			list2 = append(list2, fieldToInt2)
+
+		}
+		if err != nil {
+			break
+		}
+	}
+
+	slices.Sort(list1)
+	slices.Sort(list2)
 
 	var result float64
-	for i := 0; i < len(nums1); i++ {
-		result += (math.Abs(float64(nums1[i]) - float64(nums2[i])))
+	for i := 0; i < len(list1); i++ {
+		result += (math.Abs(float64(list1[i]) - float64(list2[i])))
 	}
 
 	fmt.Println(result)
-
-	//TODO: read data from csv file
 
 }
